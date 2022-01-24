cc3ae698ce4ba2b679e45f8f6756511193256700
Gaurav Kathar <gkathar221@gmail.com>
2022-01-25 01:18:07 +0530

    User Profile and Pics added

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)   # cascade means that if user is deleted then delete
     # profile but not vice-versa
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
