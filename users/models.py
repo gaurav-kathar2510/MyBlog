492f796e999525d6c0a6759d755f2a696e66eee8
Gaurav Kathar <gkathar221@gmail.com>
2022-01-24 23:55:18 +0530

    unresolved attribute error

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)   # cascade means that if user is deleted then delete
+    # profile but not vice-versa
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
