117e230fcd8f850e1a02c295b0fd3deb11744d09
Gaurav Kathar <gkathar221@gmail.com>
2022-01-26 00:02:08 +0530

    profile picture updation added

--- users/models.py
+++ users/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
+
 # Create your models here.
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)   # cascade means that if user is deleted then delete
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # cascade means that if user is deleted then delete
     # profile but not vice-versa
     image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_sie = (300, 300)
+            img.thumbnail(output_sie)
+            img.save(self.image.path)
